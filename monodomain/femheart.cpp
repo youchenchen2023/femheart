86741865b40182fde9ca42387b0ad40e4513ebae
youchen <youchenchen2023@gmail.com>
2023-12-13 18:34:44 +0800

    w

--- monodomain/femheart.cpp
+++ monodomain/femheart.cpp
@@ -84,7 +84,8 @@ int main(int argc, char *argv[])
    args.ParseCheck();
    
    if (my_rank == 0)
-   {
+   {  
+      cout<<"argc:"<<argc<<endl;
       for(int i = 0; i< argc; ++i)
       {
          std::cout<<"!"<<argv[i]<<std::endl;
